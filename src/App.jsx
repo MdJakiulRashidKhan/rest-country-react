b8d523a26479cefaa9fc5f6227804d3188147608
Soraz <you@example.com>
2024-12-28 11:20:50 +0600

    done

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Countries from "./Countries";
+
+function App() {
+  return (
+    <div>
+      <Countries></Countries>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Countries.jsx
@@ -0,0 +1,25 @@
+import React, { useEffect, useState } from "react";
+import "./Countries.css";
+import Country from "./Country";
+
+const Countries = () => {
+  const [countries, setCountries] = useState([]);
+  useEffect(() => {
+    fetch("https://restcountries.com/v3.1/all")
+      .then((res) => res.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  return (
+    <div>
+      <h1>Countries : {countries.length}</h1>
+      <div className="country-container">
+        {countries.map((country) => (
+          <Country key={country.cca3} country={country}></Country>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Countries;
